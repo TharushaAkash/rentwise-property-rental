ae3272c0cd72b5cd43a529c5a8dfa9cd8488dbab
TharushaAkash <tharushaakasha22@gmail.com>
2026-09-15 10:08:24 +0530

    feat(property): add CreatePropertyDto to encapsulate property creation fields

---
+++ RentWise-Backend/Modules/PropertyManagement/DTOs/CreatePropertyDto.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using RentWise_Backend.Models.PropertyManagement;
+
+namespace RentWise_Backend.Modules.PropertyManagement.DTOs
+{
+    public class CreatePropertyDto
+    {
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        public decimal MonthlyRent { get; set; }
+
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        
+        public string Facilities { get; set; } = string.Empty;
+
+        public int Sqft { get; set; }
+        public string PropertyType { get; set; } = string.Empty;
+        public List<string> FeatureTags { get; set; } = new List<string>();
+        public int PhotoCount { get; set; }
+    }
+}
